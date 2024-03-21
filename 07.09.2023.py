d73dfeb78cd49962bf86bcb8283dfae175fb8954
Varvara5932 <164366659+Varvara5932@users.noreply.github.com>
2024-03-21 16:55:58 +0200

    Add files via upload

---
+++ 07.09.2023.py
@@ -0,0 +1,10 @@
+with open('9.Txt', 'a', encoding='UTF-8') as file:
+    file.write('Пивіт, мене звати ?')
+with open('9.Txt', 'a', encoding='UTF-8') as file:
+    file.write('Пивіт, мене звати ?')
+with open('9.Txt', 'a', encoding='UTF-8') as file:
+    file.write('Пивіт, мене звати ?')
+with open('9.Txt', 'a', encoding='UTF-8') as file:
+    file.write('Пивіт, мене звати ?')
+with open('9.Txt', 'a', encoding='UTF-8') as file:
+    file.write('Пивіт, мене звати ?')
